2aa77d23c57291fe54e97f379baa7b69c3548957
David Austin <DavidWayneAustin@outlook.com>
2020-01-09 12:51:19 +0000

    Adding data entry to combo box simple

--- WpfTestApp/MainWindow.xaml.cs
+++ WpfTestApp/MainWindow.xaml.cs
@@ -26,9 +26,9 @@ namespace WpfTestApp
         {
             InitializeComponent();
 
-            people.Add(new Person { FirstName = "John", LastName = "Smith" });
-            people.Add(new Person { FirstName = "Jo", LastName = "Smith" });
-            people.Add(new Person { FirstName = "Jeff", LastName = "Smith" });
+            people.Add(new Person { FirstName = "John", LastName = "Smith", Age = "20", Message = "None" });
+            people.Add(new Person { FirstName = "Jo", LastName = "Smith", Age = "20", Message = "None" });
+            people.Add(new Person { FirstName = "Jeff", LastName = "Smith", Age = "20", Message = "None" });
 
             myComboBox.ItemsSource = people;
         }
@@ -36,6 +36,8 @@ namespace WpfTestApp
         private void SubmitButton_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show($"Enterred data {firstNameText.Text}");
+            people.Add(new Person { FirstName = $"{firstNameText.Text}", LastName = $"{lastNameText.Text}", Age = $"{ageText.Text}", Message = $"{messageText.Text}" });
+
         }
     }
 
@@ -43,5 +45,7 @@ namespace WpfTestApp
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Age { get; set; }
+        public string Message { get; set; }
     }
 }
